ce2812c29c2d21df8b0535215c91fe57ff0b3ac6
Frédéric Santos <frederic.santos@u-bordeaux.fr>
2019-06-27 07:37:47 +0200

    Rename select_vars -> select_traits

--- R/select_traits.R
+++ R/select_traits.R
@@ -1,4 +1,4 @@
-select_vars <- function(tab, k = 10, excludeTraits = c("none", "excludeNPT", "excludeQNPT", "excludeNCT"), OMDvalue = NULL, groups, formule = c("Anscombe", "Freeman")) {
+select_traits <- function(tab, k = 10, excludeTraits = c("none", "excludeNPT", "excludeQNPT", "excludeNCT"), OMDvalue = NULL, groups, formule = c("Anscombe", "Freeman")) {
 ### tab: table of sample sizes and frequencies
 ### k: numeric value, required minimal number of individuals per group
 ### excludeTraits: scheme of exclusion of non-polymorphic traits
