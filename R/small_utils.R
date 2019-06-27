1f951b59ba801709b19478c5e6d8a5af29571230
Frédéric Santos <frederic.santos@u-bordeaux.fr>
2019-06-27 12:02:46 +0200

    Define new mds_rho function

--- R/small_utils.R
+++ R/small_utils.R
@@ -1,4 +1,5 @@
 theta <- function(n, p, choice = c("Anscombe", "Freeman")) {
+### Function for angular transformation
 ### n: sample size
 ### p: relative frequency for the given trait
 ### choice: variant of angular transformation to be used
@@ -16,9 +17,21 @@ sd_mmd <- function(nA, nB) {
 }
                            
 compute_md <- function(nA, pA, nB, pB, choice = c("Anscombe", "Freeman")) {
+### Computes the measure of divergence for one given trait
 ### nA & nB: sample sizes in the groups A et B
 ### pA & pB: trait frequencies in the groups A et B
 ### choice: variant of angular transformation to be used    
-### Computes the measure of divergence for one given trait
     return((theta(nA, pA, choice = choice) - theta(nB, pB, choice = choice))^2 - sqrt(sd_mmd(nA, nB)))
 }   
+
+mds_rho <- function(mmd, coor, k) {
+### Computes the rho values displayed on MDS plots
+### mmd: symmetrical matrix of MMD values
+### coor: MDS coordinates
+### k: dimension (2 or 3, for 2D or 3D plots)
+    mds_distances <- dist(coor[ , 1:k], diag = FALSE, upper = FALSE) # compute pairwise distances between all individuals on the MDS plot
+    mmd_distances <- as.dist(mmd, diag = FALSE, upper = FALSE) # MMD dissimilarities
+    ## Compute correlation between "real" (MMD) distances, and the distances post-MDS, not taking into account the null diagonal:
+    rho <- cor(as.vector(as.matrix(mmdDistances)), as.vector(as.matrix(mdsDistances)), method = "spearman")
+    return(round(rho, 3))
+}
