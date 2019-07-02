fd9e2055cfb50bb362585909edf78eeb326f43cb
Frédéric Santos <frederic.santos@u-bordeaux.fr>
2019-07-02 13:02:00 +0200

    Create start_mmd alias for StartMMD function

--- R/StartMMD.R
+++ R/StartMMD.R
@@ -1,8 +1,13 @@
-StartMMD <- function() {
+start_mmd <- function() {
     ## Define the UI and server files for the app:
     app <- shiny::shinyApp(ui = ui, server = server)
     ## Define a folder that contains a CSS sheet:
     shiny::addResourcePath(prefix = "style", directoryPath = system.file("css", package = "AnthropMMD"))
     ## Run the app:
     shiny::runApp(app)
+}
+
+StartMMD <- function() {
+    .Deprecated("start_mmd")
+    start_mmd()
 }
