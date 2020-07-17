9ffb722f7924b311435f9bdb24f0a0970a1e829a
f-santos <frederic.santos@u-bordeaux.fr>
2020-07-17 10:38:46 +0200

    Improved code structure

--- R/table_relfreq.R
+++ R/table_relfreq.R
@@ -1,7 +1,20 @@
 table_relfreq <- function(tab) {
-## tab: table of sample sizes and raw frequencies, such as returned by 'binary_to_table'
-## Converts the *K* last rows of tab into relative frequencies.
-    nb_groupes <- nrow(tab)/2 # number of groups
-    tab[(nb_groupes+1):nrow(tab), ] <- tab[(nb_groupes+1):nrow(tab), ] / tab[1:nb_groupes, ] # convert raw frequencies into relative frequencies
+### tab: table of sample sizes and raw frequencies,
+###      such as returned by `binary_to_table()'
+### Converts the `N/2' last rows of tab into relative frequencies.
+
+    ## Useful constant:
+    n <- nrow(tab)
+
+    ## Indices of last N/2 rows:
+    last_rows <- seq(from = n / 2 + 1, to = n, by = 1)
+
+    ## Indices of first N/2 rows:
+    first_rows <- seq_len(n / 2)
+
+    ## Convert raw frequencies into relative frequencies:
+    tab[last_rows, ] <- tab[last_rows, ] / tab[first_rows, ]
+
+    ## Return result:
     return(tab)
 }
