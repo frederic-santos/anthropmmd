75587ea6d7ed9922251d744d2510c1d98e26b6a2
f-santos <frederic.santos@u-bordeaux.fr>
2020-07-17 17:03:23 +0200

    Fix some problems of definition of anthropmmd_result class

--- R/plot.anthropmmd_result.R
+++ R/plot.anthropmmd_result.R
@@ -2,13 +2,14 @@ plot.anthropmmd_result <- function(x,
                                    method = c("classical", "interval", "ratio", "ordinal"),
                                    axes = FALSE, gof = FALSE,
                                    dim = 2, asp = TRUE,
-                                   xlim = NULL) {
+                                   xlim = NULL, ...) {
 ### x: an object of class "anthropmmd_result"
 ### method: type of MDS. "classical" for cmdscale, or one of SMACOF methods
 ### axes: boolean, display axes on plot or not
 ### gof: boolean, display goodness of fit value on plot or not
 ### dim: *maximal* dimension for the calculation of MDS coordinates. /!\ In fine, the solution can be computed on 2 axes only even if dim=3.
 ### asp: boolean, TRUE passes "asp=1" to plot function.
+### ...: possibly other parameters passed to plot()
 
     ## Check input:
     stopifnot(class(x) == "anthropmmd_result")
