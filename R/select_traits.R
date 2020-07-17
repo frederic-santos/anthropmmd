2be2e9d18c3b3b910829b8547de6dde134280cb0
f-santos <frederic.santos@u-bordeaux.fr>
2020-07-17 10:32:07 +0200

    Improved code readability

--- R/select_traits.R
+++ R/select_traits.R
@@ -1,78 +1,102 @@
-select_traits <- function(tab, k = 10, strategy = c("none", "excludeNPT", "excludeQNPT", "excludeNOMD", "keepFisher"), OMDvalue = NULL, groups = NULL, angular = c("Anscombe", "Freeman")) {
+select_traits <- function(tab, k = 10,
+                          strategy = c("none", "excludeNPT", "excludeQNPT", "excludeNOMD", "keepFisher"),
+                          OMDvalue = NULL, groups = NULL,
+                          angular = c("Anscombe", "Freeman")) {
 ### tab: table of sample sizes and frequencies
 ### k: numeric value, required minimal number of individuals per group
 ### strategy: scheme of exclusion of non-polymorphic traits
-### groups: a factor or character vector, indicating the group to be considered in the analysis
+### groups: a factor or character vector, indicating the groups to be included
 ### angular: angular transformation to be used in MMD formula
 
-    strategy <- match.arg(strategy) # to avoid a warning if the user does not specify anything
-    angular <- match.arg(angular) # idem
-    
-#############################################################
-### 1. Select a subset of the dataset according to the groups
-    nbGroupes <- nrow(tab) / 2 # *initial* number of groups
-    if (is.null(groups)) { # if the user did not specify any group, take all of them
-        etiquettes <- rownames(tab)[1:nbGroupes]
+    strategy <- match.arg(strategy) # avoid a warning if the user gives no arg
+    angular <- match.arg(angular)   # idem
+
+    ###############################################################
+    ## 1. Select a subset of the dataset according to the groups ##
+    ###############################################################
+    nb_groups <- nrow(tab) / 2 # *initial* number of groups, before subsetting
+    if (is.null(groups)) {     # if the user specified nothing, keep all groups
+        etiquettes <- rownames(tab)[1:nb_groups]
         groups <- factor(substr(etiquettes, 3, nchar(etiquettes)))
     }
-    groupeOK <- substr(rownames(tab), 3, nchar(rownames(tab))) %in% as.character(groups) # boolean, indicated whether each group is to be considered or not
-    groupeOK[(nrow(tab)/2 + 1):nrow(tab)] <- groupeOK[1:(nrow(tab)/2)]
-    tab <- tab[groupeOK, ]
-    nbGroupes <- nrow(tab) / 2 # *final* number of groups after subsetting
+    ## A boolean indicating whether each group sould be included:
+    take_group <- substr(rownames(tab), 3, nchar(rownames(tab))) %in% as.character(groups)
+    ## Perform subsetting in data to keep only those groups:
+    take_group[(nrow(tab) / 2 + 1):nrow(tab)] <- take_group[1:(nrow(tab) / 2)]
+    tab <- tab[take_group, ]
+    ## *Final* number of groups after subsetting:
+    nb_groups <- nrow(tab) / 2
 
-###############################################################################################
-### 2. Filter the dataset to keep only the traits having at least 'k' individuals in each group
+    ###########################################################################
+    ## 2. Filter data to keep only the traits with >=k individuals per group ##
+    ###########################################################################
     sel <- rep(NA, ncol(tab)) # initialize an empty boolean
-    for (j in 1:ncol(tab)) { # for each trait,
-        if (all(tab[1:nbGroupes, j] >= k) == TRUE) { # if there are more than 'k' individuals per group for this trait,
-            sel[j] <- TRUE # then keep this trait,
+    for (j in seq_len(ncol(tab))) {  # For each trait...
+        ## if there are more than 'k' individuals per group for this trait,
+        if (all(tab[1:nb_groups, j] >= k) == TRUE) {
+            sel[j] <- TRUE           # then keep this trait,
         } else {
-            sel[j] <- FALSE # else discard it.
+            sel[j] <- FALSE          # else discard it.
         }
     }
 
-    if (sum(sel) > 1) { # if there are at least 2 traits meeting this criterion, we filter the dataset,
-        tab <- tab[ , sel]
+    ## If at least 2 traits meet this criterion, filter the dataset:
+    if (sum(sel) > 1) {
+        tab <- tab[, sel]
     } else {
-        return() # else we stop here (a MMD does not make sense if there is only one available trait!)
+        ## else stop here (a MMD does not make sense if there is only one trait)
+        return()
     }
 
-####################################################################
-### 3. Addtional step to discard the traits with too few variability
-    OMDs <- compute_omd(data = tab, formule = angular, OMDvalue = OMDvalue)
-    if (strategy == "excludeNPT") { # Scheme of exclusion: exclude non-polymorphic traits
+    ######################################################################
+    ## 3. Addtional step to discard the traits with too few variability ##
+    ######################################################################
+    omd <- compute_omd(data = tab, formule = angular, OMDvalue = OMDvalue)
+
+    ## 3.1. If scheme of exclusion = exclude non-polymorphic traits:
+    if (strategy == "excludeNPT") {
         polym <- rep(NA, ncol(tab))
-        for (j in 1:ncol(tab)) {
-            polym[j] <- ifelse(all(tab[(nbGroupes+1):nrow(tab), j] == 0) | all(tab[(nbGroupes+1):nrow(tab), j] == 1), FALSE, TRUE) # TRUE iff the trait has two levels
+        for (j in seq_len(ncol(tab))) {
+            polym[j] <- ifelse(all(tab[(nb_groups+1):nrow(tab), j] == 0) | all(tab[(nb_groups+1):nrow(tab), j] == 1), FALSE, TRUE)
+            ## (this is TRUE iff the trait has two levels)
         }
-        tab <- tab[ , polym] # keep polymorphic traits only
-        tabDisplay <- OMDs$Sorted[rownames(OMDs$Sorted) %in% colnames(tab), ]
-    } 
-    else if (strategy=="excludeQNPT") { # Scheme of exclusion: "quasi-non-polymorphic" traits
-        avirer <- rep(NA, ncol(tab))
-        for (j in 1:ncol(tab)) {
-            tabprov <- round(tab[1:nbGroupes, j] * tab[(nbGroupes+1):nrow(tab), j],1)
-            tabprov <- abs(tab[1:nbGroupes, j] - tabprov) 
-            avirer[j] <- ifelse(sum(tabprov)<=1 | sum(tabprov)>=(sum(tab[1:nbGroupes, j])-1), FALSE, TRUE) 
-        }
-        tab <- tab[ , avirer]
-        tabDisplay <- OMDs$Sorted[rownames(OMDs$Sorted) %in% colnames(tab), ]
-    } 
-    else if (strategy=="excludeNOMD") { # Scheme of exclusion: weak contribution to MMD
-        tab <- tab[ , rownames(OMDs$Pos)]
-        tabDisplay <- OMDs$SortedPos
-    } 
-    else if (strategy=="keepFisher") { # Scheme of exclusion: keep only those traits that show significant differencies between groups at Fisher's exact test
-        tab <- fisherTestTab(tab)$informative
-        tabDisplay <- OMDs$Sorted[rownames(OMDs$Sorted) %in% colnames(tab), ]
-        
-    } else { # Scheme of exclusion: "none", no particular criterion -> no filtering at all.
-        tabDisplay <- OMDs$Sorted
+        tab <- tab[, polym] # keep polymorphic traits only
+        tab_display <- omd$Sorted[rownames(omd$Sorted) %in% colnames(tab), ]
     }
-    
-#########################
-### 4. Return the results
-    tabDisplay <- as.matrix(tabDisplay)
-    colnames(tabDisplay) <- "Overall MD"
-    return(list("filtered" = tab, "OMD" = tabDisplay))
+
+    ## 3.2. If scheme of exclusion = "quasi-non-polymorphic" traits:
+    else if (strategy == "excludeQNPT") {
+        avirer <- rep(NA, ncol(tab))
+        for (j in seq_len(ncol(tab))) {
+            tabprov <- round(tab[1:nb_groups, j] * tab[(nb_groups + 1):nrow(tab), j], 1)
+            tabprov <- abs(tab[1:nb_groups, j] - tabprov)
+            avirer[j] <- ifelse(sum(tabprov) <= 1 | sum(tabprov) >= (sum(tab[1:nb_groups, j])-1), FALSE, TRUE)
+        }
+        tab <- tab[, avirer]
+        tab_display <- omd$Sorted[rownames(omd$Sorted) %in% colnames(tab), ]
+    }
+
+    ## 3.3. If scheme of exclusion = weak contribution to MMD:
+    else if (strategy == "excludeNOMD") {
+        tab <- tab[, rownames(omd$Pos)]
+        tab_display <- omd$SortedPos
+    }
+
+    ## 3.4. If scheme of exclusionis based on Fisher's exact tests:
+    else if (strategy=="keepFisher") {
+        tab <- fisherTestTab(tab)$informative
+        tab_display <- omd$Sorted[rownames(omd$Sorted) %in% colnames(tab), ]
+    }
+
+    ## If scheme of exclusion = "none", then no filtering at all:
+    else {
+        tab_display <- omd$Sorted
+    }
+
+    ###########################
+    ## 4. Return the results ##
+    ###########################
+    tab_display <- as.matrix(tab_display)
+    colnames(tab_display) <- "Overall MD"
+    return(list("filtered" = tab, "OMD" = tab_display))
 }
