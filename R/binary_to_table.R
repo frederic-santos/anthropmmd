7bece6d43986dbe340f825a120b43b428b751539
f-santos <frederic.santos@u-bordeaux.fr>
2020-07-17 10:10:56 +0200

    Improved comments and indentation

--- R/binary_to_table.R
+++ R/binary_to_table.R
@@ -1,32 +1,46 @@
 binary_to_table <- function(data, relative = FALSE) {
 ### data: matrix of binary data, + one column for the group indicator
-### relative: boolean, indicates if the last rows of the table must contain frequencies (i.e., number of individuals having a given trait) or relative frequencies (i.e., proportions)
+### relative: boolean, indicates whether the last rows of the table contain
+###           absolute frequencies (i.e., number of individuals with each trait)
+###           or relative frequencies (i.e., proportions).
 ### Converts this data frame into a table of group sample sizes and frequencies.
 
-    ## 1. Set some constants:
-    nb_traits <- ncol(data) - 1 # number of traits (first column does not count since it is the group indicator)
-    groups <- factor(data[ , 1]) # group indicator in the input dataset
+    ###########################
+    ## 1. Set some constants ##
+    ###########################
+    nb_traits <- ncol(data) - 1  # 1st column is a group indicator, not a trait
+    groups <- factor(data[, 1])  # group indicator in the input dataset
     nb_groups <- nlevels(groups) # number of groups
     group_names <- levels(groups)
 
-    ## 2. Initialize an empty matrix in which to fill the results:
-    MatRes <- matrix(nrow = 2*nb_groups, ncol = nb_traits)
-    colnames(MatRes) <- colnames(data)[-1]
-    rownames(MatRes) <- c(paste("N_", group_names, sep=""), paste("Freq_", group_names, sep=""))
-    
-    ## 3. Fill in the matrix:
-    for (j in 1:nb_traits) { # For each trait...
-        x <- split(data[ , j+1], groups) # ... split the current trait values according to the groups.
-        for (i in 1:nb_groups) { # Then for each group...
-            MatRes[i, j] <- sum(is.na(x[[i]]) == FALSE) # compute the number of nonmissing values...
-            MatRes[i+nb_groups, j] <- sum(x[[i]] == 1, na.rm = TRUE) # and the relative frequency for the current trait.
+    ###################################################
+    ## 2. Initialize an empty matrix for the results ##
+    ###################################################
+    mat <- matrix(nrow = 2 * nb_groups, ncol = nb_traits)
+    colnames(mat) <- colnames(data)[-1]
+    rownames(mat) <- c(paste("N", group_names, sep = "_"),
+                       paste("Freq", group_names, sep = "_"))
+
+    ###########################
+    ## 3. Fill in the matrix ##
+    ###########################
+    for (j in seq_len(nb_traits)) { # For each trait...
+        ## ... split the current trait values according to the groups:
+        x <- split(data[, j + 1], groups)
+        for (i in 1:nb_groups) { # And then for each group...
+            ## ... compute the number of nonmissing values:
+            mat[i, j] <- sum(is.na(x[[i]]) == FALSE)
+            ## and the relative frequency for the current trait:
+            mat[i + nb_groups, j] <- sum(x[[i]] == 1, na.rm = TRUE)
         }
     }
 
-    ## 4. Return the results:
+    ###########################
+    ## 4. Return the results ##
+    ###########################
     if (relative == FALSE) {
-        return(MatRes)
+        return(mat)
     } else {
-        return(table_relfreq(MatRes))
+        return(table_relfreq(mat))
     }
 }
