c751bf3f50a785289fca3e691384ac1288470558
f-santos <frederic.santos@u-bordeaux.fr>
2020-07-20 09:11:13 +0200

    More unit tests for p-values

--- tests/testthat/test-mmd.R
+++ tests/testthat/test-mmd.R
@@ -12,52 +12,56 @@ mmd_ans <- mmd(tab, angular = "Anscombe")
 mmd_ft <- mmd(tab, angular = "Freeman")
 
 ## Expected result when using Anscombe transformation:
-res.ans <- matrix(c(0.000, 0.255, 0.045, 0.303,	0.069,
+res_ans <- matrix(c(0.000, 0.255, 0.045, 0.303,	0.069,
                     0.255, 0.000, 0.218, 0.365, 0.241,
                     0.045, 0.218, 0.000, 0.088, 0.034,
                     0.303, 0.365, 0.088, 0.000,	0.168,
                     0.069, 0.241, 0.034, 0.168,	0.000),
                   byrow = TRUE, ncol = 5)
-colnames(res.ans) <- c("GroupA", "GroupB", "GroupC", "GroupD", "GroupE")
-rownames(res.ans) <- c("GroupA", "GroupB", "GroupC", "GroupD", "GroupE")
+colnames(res_ans) <- c("GroupA", "GroupB", "GroupC", "GroupD", "GroupE")
+rownames(res_ans) <- c("GroupA", "GroupB", "GroupC", "GroupD", "GroupE")
 
 ## Expected result when using Freeman-Tukey transformation:
-res.ft <- matrix(c(0.000, 0.271, 0.042,	0.298, 0.064,
+res_ft <- matrix(c(0.000, 0.271, 0.042,	0.298, 0.064,
                    0.271, 0.000, 0.225,	0.365, 0.251,
                    0.042, 0.225, 0.000,	0.090, 0.031,
                    0.298, 0.365, 0.090,	0.000, 0.168,
                    0.064, 0.251, 0.031,	0.168, 0.000),
                  byrow = TRUE, ncol = 5)
-colnames(res.ft) <- c("GroupA", "GroupB", "GroupC", "GroupD", "GroupE")
-rownames(res.ft) <- c("GroupA", "GroupB", "GroupC", "GroupD", "GroupE")
+colnames(res_ft) <- c("GroupA", "GroupB", "GroupC", "GroupD", "GroupE")
+rownames(res_ft) <- c("GroupA", "GroupB", "GroupC", "GroupD", "GroupE")
 
 ## Expected result for MMD significance:
-res.sig <- matrix(c(NA, "0.255", "0.045", "0.303", "0.069",
+res_sig <- matrix(c(NA, "0.255", "0.045", "0.303", "0.069",
                     "*", NA, "0.218", "0.365", "0.241",
                     "NS", "*", NA, "0.088", "0.034",
                     "*", "*", "*", NA, "0.168",
                     "NS", "*", "NS", "*", NA),
                   byrow = TRUE, ncol = 5)
-colnames(res.sig) <- c("GroupA", "GroupB", "GroupC", "GroupD", "GroupE")
-rownames(res.sig) <- c("GroupA", "GroupB", "GroupC", "GroupD", "GroupE")
+colnames(res_sig) <- c("GroupA", "GroupB", "GroupC", "GroupD", "GroupE")
+rownames(res_sig) <- c("GroupA", "GroupB", "GroupC", "GroupD", "GroupE")
 
 ############################
 ### Tests for MMD values ###
 ############################
 test_that("MMD values are correct with Anscombe transformation", {
-    expect_equal(round(mmd_ans$MMDSym, 3), res.ans)
+    expect_equal(round(mmd_ans$MMDSym, 3), res_ans)
 })
 
 test_that("MMD values are correct with Freeman-Tukey transformation", {
-    expect_equal(round(mmd_ft$MMDSym, 3), res.ft)
+    expect_equal(round(mmd_ft$MMDSym, 3), res_ft)
 })
 
 ## Tests for MMD significance:
 test_that("MMD significance is OK", {
-    expect_equal(mmd_ans$MMDSignif, res.sig)
+    expect_equal(mmd_ans$MMDSignif, res_sig)
 })
 
 ## Tests for p-values:
-test_that("p-values are OK", {
+test_that("p-value C-E is OK", {
     expect_equal(mmd_ans$MMDpval[5, 3], 0.0954)
+})
+
+test_that("p-value A-E is OK", {
+    expect_equal(mmd_ans$MMDpval[5, 1], 0.2638)
 })
